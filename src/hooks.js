f2c1963cc2aad7df03889056a751ac3d8eb1cf95
ayush <58028488+desibabua@users.noreply.github.com>
2020-09-22 22:55:08 +0530

    refactored code

--- src/hooks.js
+++ src/hooks.js
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react';
-import {useHistory} from 'react-router-dom';
-import {getFetch} from './api/reqAPIs'
+import { useHistory } from 'react-router-dom';
+import { getFetch } from './api/reqAPIs';
 
 const useUser = () => {
   const history = useHistory();
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    getFetch('/getUser')
-      .then(({ user }) => setUser(user));
+    getFetch('/getUser').then(({ user }) => setUser(user));
   }, [history]);
 
   return user;
